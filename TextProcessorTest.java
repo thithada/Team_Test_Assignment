e3ed3917ebc65ee4b7f53ad5986ee0a135dcadb4
thithada <150765761+thithada@users.noreply.github.com>
2024-12-23 13:32:53 +0700

    more Test

--- TextProcessorTest.java
+++ TextProcessorTest.java
@@ -6,10 +6,12 @@ class TextProcessorTest {
     @Test
     void testConvertAgeToBuddhistEra() {
         assertEquals("2547", TextProcessor.convertAgeToBuddhistEra(20));
+        assertEquals("2543", TextProcessor.convertAgeToBuddhistEra(24));
     }
 
     @Test
     void testConvertGradeToRank() {
         assertEquals("High Distinction", TextProcessor.convertGradeToRank("A"));
+        assertEquals("Failed", TextProcessor.convertGradeToRank("F"));
     }
 }
